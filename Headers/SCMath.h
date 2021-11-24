18fcedb40c5ba1e5ce58828190867023f04fb00d
davis <itsmeeugarps@gmail.com>
2021-11-24 20:10:44 +0900

    Add clip method

--- Headers/SCMath.h
+++ Headers/SCMath.h
@@ -16,4 +16,10 @@ namespace SCMath
 {
 #define PI 3.14159265358979323846
 #define HALF_PI 1.57079632679489661923
+
+    inline float clip(float value, float min, float max)
+    {
+        value = (value < min) ? min : value;
+        return (value > max) ? max : value;
+    }
 }
